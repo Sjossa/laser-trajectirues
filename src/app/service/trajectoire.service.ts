24ca2766ec2e653bb0e5f96eb7afb5f27042ba9f
Johnny Sassiat <johnny.sass2001@gmail.com>
2026-02-12 21:18:36 +0100

    x

--- src/app/service/trajectoire.service.ts
+++ src/app/service/trajectoire.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { TrajectoireData } from '../model/Trajectoire.model';
+import { TrajectoireData } from '../model/trajectoire.model';
 
 @Injectable({
   providedIn: 'root',
