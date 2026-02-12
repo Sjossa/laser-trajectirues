37c67100e162e899f12eb4d0b7f8e5d0e4ebb3fd
Johnny Sassiat <johnny.sass2001@gmail.com>
2026-02-12 20:54:38 +0100

    creation service pour lire le fichier json

---
+++ src/app/service/trajectoire.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { TrajectoryData } from '../model/Trajectoire.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TrajectoryService {
+  private dataUrl = 'assets/trajectorie.json';
+
+  constructor(private http: HttpClient) {}
+
+  getTrajectoire(): Observable<TrajectoryData> {
+    return this.http.get<TrajectoryData>(this.dataUrl);
+  }
+}
